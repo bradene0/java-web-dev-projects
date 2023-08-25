22306bea6df1cc04f1b9e40da4680e9bf48f1c3b
Braden E <bradene0@outlook.com>
2023-08-24 19:46:48 -0500

    completed studio and bonus

---
+++ control-flow-and-collections/studio/src/CountingCharacters.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CountingCharacters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a message to test: ");
+        String input = scanner.nextLine().trim();
+
+            if (input.isEmpty()) {
+                System.out.println("Error! No message detected!");
+            }
+
+
+        // Stores and updates the count
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+
+        // Loops through the string one char at a time
+        for (char currentChar : input.toCharArray()) {
+            if (Character.isLetterOrDigit(currentChar)) {
+                charCountMap.put(currentChar, charCountMap.getOrDefault(currentChar, 0) + 1);
+            }
+
+        }
+
+        // Loops through the HashMap to print the results
+        for (char currentChar : charCountMap.keySet()) {
+            System.out.println("Character " + currentChar + ", Count: " + charCountMap.get(currentChar));
+        }
+        System.out.println("Message characters counted successfully!");
+        scanner.close();
+    }
+}
+
